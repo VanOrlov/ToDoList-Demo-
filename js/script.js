a06a3d752d98de12c42d6048c9e6f934c4d54da2
VanOrlov <108752579+VanOrlov@users.noreply.github.com>
2022-12-13 16:59:05 +0300

    ToDoList-Demo-
    ToDoList-Demo-

--- js/script.js
+++ js/script.js
@@ -1,2 +1,36 @@
+let btn = document.querySelector('.addToDo')
+let child = document.querySelector("li")
+let ul = document.querySelector(".toDoList");
+let inp = document.querySelector('.input-todo')
 
+let array = []
 
+btn.addEventListener('click', () => {
+  if (inp.value){
+    let item = {
+      text: document.querySelector('.input-todo').value,
+      id: Math.random()
+    }
+    array.push(item)
+    console.log(array);
+    addLi()
+  }else{
+    alert('Введите значение')
+  }
+})
+
+function addLi(){
+  let li = document.createElement("li");
+  array.forEach(item => {
+    li.innerHTML = item.text
+    inp.value = ''
+    li.onclick = () => remove(item.id, li)
+    ul.appendChild(li);
+  })
+}
+
+function remove(id, li){
+  li.remove()
+  array = array.filter(item => item.id !== id)
+  console.log(array);
+}
